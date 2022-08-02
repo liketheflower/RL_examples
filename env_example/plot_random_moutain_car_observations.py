c69f26eec786e0818f2f745490c4dcd701b47666
jimmy shen <jimmy@takemobi.com>
2022-08-01 20:43:26 -0400

    Plot random moutain car observations

---
+++ env_example/plot_random_moutain_car_observations.py
@@ -0,0 +1,21 @@
+import gym
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make("MountainCar-v0")
+env.reset()
+observations = []
+for t in range(1000):
+    # env.render()
+    observation = env.reset()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    observations.append(observation)
+    if done:
+        print("Episode finished after {} timesteps".format(t + 1))
+observations = np.array(observations)
+plt.scatter(observations[:, 0], observations[:, 1], alpha=0.3)
+plt.savefig("observations.png")
+plt.show()
+env.close()
